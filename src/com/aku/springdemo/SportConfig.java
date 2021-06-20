b05258038c734457b58ce3d0a7e3eb8e82729dbf
Akki19hzb <getakash.verma19@gmail.com>
2021-06-20 14:20:24 +0530

    comment added in SportConfig class for explanation

--- src/com/aku/springdemo/SportConfig.java
+++ src/com/aku/springdemo/SportConfig.java
@@ -6,7 +6,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-//@ComponentScan("com.aku.springdemo")
+//to remove the use of XML file we used ComponentScan which created bean automatically for all the classes using @Component
+//@ComponentScan("com.aku.springdemo")  
 @PropertySource("classpath:sports.properties")
 public class SportConfig {
 	/*
